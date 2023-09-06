330e2ada52e86733d2ef1d7fabea8b7117e802bd
Sebastian Sala <sebastian_orko@hotmail.com>
2023-09-05 22:48:22 -0600

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,30 @@
+export const frontendIcons = [
+    { src: "vscode-icons:file-type-css", width: "50" },
+    { src: "vscode-icons:file-type-scss2", width: "50" },
+    { src: "skill-icons:tailwindcss-dark", width: "50" },
+    { src: "devicon:javascript", width: "50" },
+    { src: "vscode-icons:file-type-typescript-official", width: "50" },
+    { src: "logos:react", width: "50" },
+    { src: "logos:nextjs", width: "90" },
+    { src: "vscode-icons:file-type-astro", width: "50" },
+    { src: "devicon:angular", width: "60" },
+  ]
+  
+  export const backendIcons = [
+    { src: "logos:nodejs", width: "50" },
+    { src: "logos:firebase", width: "35" },
+    { src: "devicon:supabase", width: "50" },
+    { src: "skill-icons:mongodb", width: "50" },
+    { src: "skill-icons:prisma", width: "50" },
+    { src: "devicon:mongoose", width: "100" },
+  ]
+  
+  export const toolsIcons = [
+    { src: "devicon:git", width: "50", name: "git" },
+    { src: "mdi:github", width: "60", name: "github" },
+    { src: "logos:webpack", width: "45", name: "webpack" },
+    { src: "devicon:postman", width: "50", name: "postman" },
+  ]
+  
+  export const projects = [{}]
+  
